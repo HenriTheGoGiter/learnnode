395ef9e133252bb60266b7943f57c1fe67e2bdfc
henrithegogiter <henrieric.roden@tptlive.ee>
2024-10-09 11:24:34 +0300

    Numbers operations

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let a=3; // number
 let b=1.2; // number
 let c= 'hello world'; // string
@@ -12,4 +13,19 @@ let l=Math.sqrt(-1); // "Math" peab olema suure tähega algul. (NaN=Not a Number
 let m=[1, 1.2, 'asdasd', true]; // array (object)
 let n={name: 'henri', age: 18}; // object
 console.log(typeof m)
-console.log(typeof n)
+console.log(typeof n) */
+let num = 3;
+num=num+2;
+num+=2;
+num-=3;
+num*=4;
+num/=2;
+num = 7 % 2; // moodulo
+num= 6%4
+num%=2;
+num++; //lisab ühe võrra
+num--; // vähendada väärtust ühe võrra
+num=Math.PI;
+num=Math.abs (-2);
+num=2**5
+console.log(num);
