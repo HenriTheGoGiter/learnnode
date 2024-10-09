0f9e24f75ca1b48a775dbd136032124b7824070b
henrithegogiter <henrieric.roden@tptlive.ee>
2024-10-09 10:58:39 +0300

    Learn what kinds of Let variatios there are.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let a=3;
-
-//var b=1.2;
-console.log(a)
+let a=3; // number
+let b=1.2; // number
+let c= 'hello world'; // string
+let d= "hello world"; // string
+let e=true; // boolean
+let f=false; // boole
+let g=null; // väärtus, mis näitab väärtuse puudumist. (object ?)
+let h=undefined; // undefined
+let j=1/0; //Infinity type number
+let k=-1/0; //-Infinity type number
+let l=Math.sqrt(-1); // "Math" peab olema suure tähega algul. (NaN=Not a Number)
+let m=[1, 1.2, 'asdasd', true]; // array (object)
+let n={name: 'henri', age: 18}; // object
+console.log(typeof m)
+console.log(typeof n)
