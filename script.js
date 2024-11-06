3e1494db689bc4eeb55cfecd04441c1d8150867e
henrithegogiter <henrieric.roden@tptlive.ee>
2024-11-06 10:56:04 +0200

    Buttons, andf elements

--- script.js
+++ script.js
@@ -304,3 +304,18 @@ console.log(person.getBirthYear());
 console.log(person1);
 console.log();
 */
+
+// HTML algab pihta!
+let button = document.querySelector('button');
+button.addEventListener('click', function (event){
+    console.log(event);
+    let heading = document.querySelector('h1');
+    heading.innerHTML = 'Hello VM23';
+});
+
+let input = document.querySelector('input');
+input.addEventListener('input', function(event){
+let text = document.querySelector ('h1#text');
+text.innerHTML = input.value.split('').reverse().join('');
+});
+
