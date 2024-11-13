8bcedf75355a576bdc895b889db54b97409b9103
henrithegogiter <henrieric.roden@tptlive.ee>
2024-11-13 10:27:22 +0200

    Learn canvas

--- script.js
+++ script.js
@@ -318,7 +318,7 @@ input.addEventListener('input', function(event){
 let text = document.querySelector ('h1#text');
 text.innerHTML = input.value.split('').reverse().join('');
 });
-*/
+
 
 let button = document.querySelector('button');
 let input = document.querySelector('input');
@@ -347,4 +347,18 @@ button.addEventListener('mouseover', () => {
 
 button.addEventListener('mouseleave', () => {
     button.disabled = false;
-});
+}); */
+
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+context.moveTo(0,0);
+context.lineTo(300,300)
+context.stroke();
+
+context.beginPath();
+context.arc(600, 600, 50, 0, 2*Math.PI);
+context.stroke();
+
+context.font = "50px Arial";
+context.fillText('WOW CANVAS!', 600, 100);
+context.strokeText('WOW CANVAS!', 600, 200);
