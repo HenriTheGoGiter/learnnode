4ec86e96eb2d1aa9bb8a2259a90ac06c626fcb56
henrithegogiter <henrieric.roden@tptlive.ee>
2024-11-20 11:30:30 +0200

    Make Very Simple express server

--- script.js
+++ script.js
@@ -425,7 +425,22 @@ color.addEventListener('input', () => {
     context.fillStyle = color.value;
 }); */ 
 
-import common from ('./common.js');
-common.hello();
-import es from './es.js';
-es.hello();
+import express from 'express';
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(import.meta.dirname + '/views/index.html');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(import.meta.dirname + '/views/about.html');
+});
+
+app.get('/hello', (req, res) => {
+    res.sendFile(import.meta.dirname + '/views/about.html');
+});
+
+  app.listen(port, () => {
+    console.log(`example app listening on port http://localhost:${port}`)
+  });
